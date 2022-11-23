e9040da3436596d11dc9ca6bfe4087c05fd35e1f
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2022-11-23 13:53:40 -0300

    M01S04D01 Adiciona exercício 1

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-01/script.js
@@ -0,0 +1,15 @@
+const myName = 'Eduardo';
+const birthCity = 'Pitanga';
+let birthYear = 1999;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = Guarapuava;
+
+console.log(birthCity);
+
+//O erro está acontecendo porque birthCity é uma constante, então seu valor não pode ser alterado.
