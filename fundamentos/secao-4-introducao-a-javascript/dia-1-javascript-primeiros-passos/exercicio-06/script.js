2204066415ac6e489e851e55ddf242afa082e574
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2022-11-25 20:55:45 -0300

    M01 S04D01 Adiciona exercício 6

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-06/script.js
@@ -0,0 +1,10 @@
+const primeiroValor = 30;
+const segundoValor = 42;
+
+if (primeiroValor > segundoValor) {
+    console.log(primeiroValor)
+} else if (segundoValor > primeiroValor) {
+    console.log(segundoValor)
+} else {
+    console.log('Os dois valores são iguais.')
+}
