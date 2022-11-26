525f356cdf4b0f89d71758b1b6d8c5e77d5487ac
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2022-11-25 21:00:02 -0300

    M01S04D01 Adiciona exercício 9

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-09/script.js
@@ -0,0 +1,11 @@
+const ladoUm = 110;
+const ladoDois = 30;
+const ladoTres = 40;
+
+const angulo = ladoUm + ladoDois + ladoTres;
+
+if (angulo === 180 && angulo > 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
