0fa0b9ca8ff72deb45a0e1c0e174ea1c9cef8e30
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2022-11-25 21:11:11 -0300

    M01D04S01 Adiciona exercício 11

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-11/script.js
@@ -0,0 +1,17 @@
+let nota = 40;
+
+if (nota >= 90 && nota >= 0 && nota <= 100) {
+    console.log('A');
+} else if (nota >= 80 && nota >= 0 && nota <= 100) {
+    console.log('B');
+} else if (nota >= 70 && nota >= 0 && nota <= 100) {
+    console.log('C');
+} else if (nota >= 60 && nota >= 0 && nota <= 100) {
+    console.log('D');
+} else if (nota >= 50 && nota >= 0 && nota <= 100) {
+    console.log('E');
+} else if (nota >= 80 && nota >= 0 && nota <= 100) {
+    console.log('F');
+} else {
+    console.log('Nota inválida!');
+}
