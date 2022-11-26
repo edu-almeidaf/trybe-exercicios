f7e3304926d02bfe543fb4dc1218058dd864fc13
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2022-11-25 21:17:31 -0300

    M01D04S01 Adiciona exercício 12

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-12/script.js
@@ -0,0 +1,19 @@
+let numero1 = 6;
+let numero2 = 2;
+let numero3 = 10;
+
+if (numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+let numero4 = 5;
+let numero5 = 2;
+let numero6 = 10;
+
+if (numero4 % 2 === 1 || numero5 % 2 === 1 || numero6 % 2 === 1) {
+    console.log(true);
+} else {
+    console.log(false);
+}
