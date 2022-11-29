0d24b1a7777a3cdeb389dd19d3e4909cbeddaa32
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2022-11-29 16:12:07 -0300

    M01S04D03 Adiciona exercício 3

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao/exercicio-03/script.js
@@ -0,0 +1,16 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+maiorPalavra = array[0];
+menorPalavra = array[0];
+
+for (index = 0; index < array.length; index += 1) {
+    if (maiorPalavra.length < array[index].length) {
+        maiorPalavra = array[index];
+    }
+
+    if (menorPalavra.length > array[index].length) {
+        menorPalavra = array[index];
+    }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
