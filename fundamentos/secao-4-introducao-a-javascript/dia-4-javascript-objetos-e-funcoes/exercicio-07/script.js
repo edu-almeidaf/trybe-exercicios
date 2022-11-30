67ac246abf982ffc468127d2cf817bb6863c9d25
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2022-11-30 19:43:36 -0300

    M01S04D04 Adiciona forma alternativa do exercício 7

--- fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-07/script.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-07/script.js
@@ -9,3 +9,23 @@ function angulosTriangulo(anguloUm, anguloDois, anguloTres) {
 }
 
 console.log(angulosTriangulo(90, 30, 60));
+
+
+// Forma 2
+
+function triangleAnglesValidate(angleA, angleB, angleC) {
+    let sumOfAngles = angleA + angleB + angleC;
+    let allAnglesArePositives = angleA > 0 && angleB > 0 && angleC > 0;
+  
+    if (allAnglesArePositives) {
+      if (sumOfAngles === 180) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return 'Erro: ângulo inválido';
+    }
+  }
+
+  console.log(triangleAnglesValidate(90, 30, 60));
