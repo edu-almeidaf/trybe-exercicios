93b3f12f88b0e2db4876b1ab01d8baa6b0d08c9b
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2022-11-30 16:13:37 -0300

    M01S04D04 Adiciona exercício 3

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-03/script.js
@@ -0,0 +1,25 @@
+function soma(a, b) {
+    return(a + b);
+}
+
+function subtracao(a, b) {
+    return(a - b);
+}
+
+function multiplicacao(a, b) {
+    return(a * b);
+}
+
+function divisao(a, b) {
+    return(a / b);
+}
+
+function modulo(a, b) {
+    return(a % b);
+}
+
+console.log(soma(20, 10));
+console.log(subtracao(20, 10));
+console.log(multiplicacao(20, 10));
+console.log(divisao(20, 10));
+console.log(modulo(20, 10));
