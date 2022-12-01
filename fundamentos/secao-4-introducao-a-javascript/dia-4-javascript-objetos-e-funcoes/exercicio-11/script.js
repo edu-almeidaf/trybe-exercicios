5c1714f557ed26840fff6d643afaf73415f60c5b
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2022-12-01 15:57:21 -0300

    M01S04D04 Adiciona segunda parte do exercício 11

--- fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-11/script.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-11/script.js
@@ -1,7 +1,7 @@
 function maiorIndice(arrayNumeros){
     let indice = 0;
 
-    for (index in arrayNumeros) {
+    for (let index in arrayNumeros) {
         
         if (arrayNumeros[index] > arrayNumeros[indice]){
             indice = index;
@@ -12,4 +12,20 @@ function maiorIndice(arrayNumeros){
     return indice;
 }
 
-console.log(maiorIndice([23, 3, 60, 7, 10, 100]));
+console.log(maiorIndice([230, 3, 60, 7, 10, 100]));
+
+
+
+function menorIndice(numeros){
+    let menorIndice = 0;
+
+    for (let index1 in numeros){
+        if (numeros[menorIndice] > numeros[index1]) {
+            menorIndice = index1;
+        }
+    }
+
+    return menorIndice;
+}
+
+console.log(menorIndice([2, 4, -6, 7, 10, 0, -3]));
