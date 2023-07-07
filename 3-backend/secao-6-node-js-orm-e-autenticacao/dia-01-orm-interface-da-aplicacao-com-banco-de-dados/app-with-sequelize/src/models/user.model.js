cf6b79bf17a36b9624cfc80d427c77d0276cae5f
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-07-07 16:15:53 -0300

    feat: dia 1 seção 6 - criado exemplo do course

---
+++ 3-backend/secao-6-node-js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-banco-de-dados/app-with-sequelize/src/models/user.model.js
@@ -0,0 +1,13 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phoneNum: DataTypes.STRING,
+  });
+  (async () => {
+    await sequelize.sync({ force: true });
+  })();
+  return User;
+}
+
+module.exports = UserModel;
