756fa2dd100850b2b69cc824a4d301b94fc8413f
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-08-27 22:57:56 -0300

    feat: Implementado exercício 4

---
+++ 3-backend/secao-9-programacao-orientada-a-objetos/dia-04-solid-principios-s-o-d/exercicio-04/src/models/interfaces/index.ts
@@ -0,0 +1,10 @@
+export { IPlant, INewPlant } from './IPlant';
+
+export {
+  IModel,
+  IModelReader,
+  IModelWriter,
+  IModelDelete,
+} from './IModel';
+
+export { default as IOpsInfo } from './IOpsInfo';
